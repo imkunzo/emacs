7e52de0efce8f4861cefd8003c3c7fa65f8ca36c
KlausZL <imkunzo@gmail.com>
2017-04-01 00:12:36 +0800

    Change fonts configuration

--- init.el
+++ init.el
@@ -52,16 +52,49 @@
   (setq nlinum-relative-current-symbol "->") ;; or "" for display current line number
   (setq nlinum-relative-offset 0)) ;; 1 if you want 0, 2, 3...
 ;;; Fonts configuration
-;; Setting english font
-(set-face-attribute
- 'default nil :font "Monaco 10")
-;; Setting chinese Font
-(if (or (eq system-type 'windows-nt) (eq system-type 'opt/cygwin))
-    (progn (dolist (charset '(kana han symbol cjk-misc bopomofo))
-             (set-fontset-font (frame-parameter nil 'font)
-                               charset
-                               (font-spec :family "Microsoft YaHei"
-                                          :size 16)))))
+;;
+(defun font-existsp (font)
+  (if (null (x-list-fonts font))
+      nil t))
+;;
+(defun make-font-string (font-name font-size)
+  (if (and (stringp font-size) 
+           (equal ":" (string (elt font-size 0))))
+      (format "%s%s" font-name font-size)
+    (format "%s %s" font-name font-size)))
+;;
+(defun set-font (english-fonts
+                 english-font-size
+                 chinese-fonts
+                 &optional chinese-font-size)
+  "english-font-size could be set to \":pixelsize=18\" or a integer. 
+  If set/leave chinese-font-size to nil, it will follow english-font-size"
+  (require 'cl)
+  (let ((en-font (make-font-string
+                  (find-if #'font-existsp english-fonts)
+                  english-font-size))
+        (zh-font (font-spec :family (find-if #'font-existsp chinese-fonts)
+                            :size chinese-font-size)))
+    ;; Set the default English font
+    ;; The following 2 method cannot make the font settig work in new frames.
+    ;; (set-default-font "Consolas:pixelsize=18")
+    ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
+    ;; We have to use set-face-attribute
+    (message "Set English Font to %s" en-font)
+    (set-face-attribute
+     'default nil :font en-font)
+    ;; Set Chinese font 
+    ;; Do not use 'unicode charset, it will cause the english font setting invalid
+    (message "Set Chinese Font to %s" zh-font)
+    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+      (set-fontset-font (frame-parameter nil 'font)
+                        charset
+                        zh-font))))
+;; setup default fonts
+(set-font
+ '("Monaco" "Ubuntu Mono" "DejaVu Sans Mono" "Consolas") ":pixelsize=16"
+ '("WenQuanYi Micro Hei Mono" "Microsoft Yahei"))
+
 ;;; long lines
 (when (require 'so-long nil :no-error)
   (so-long-enable))
