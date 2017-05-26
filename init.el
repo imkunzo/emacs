c437da29654844a7fe446bbd005f42ee4db28101
Zachariah.Li <Zack@ZackBook.local>
2017-05-26 20:00:02 +0800

    将主配置文件拆分，复制部分purcell代码，使用use-package

--- init.el
+++ init.el
@@ -1,46 +1,55 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; config customize
-(setq custom-file (expand-file-name "els/customize.el" user-emacs-directory))
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(setq custom-file (expand-file-name "lisp/customize.el" user-emacs-directory))
 (load custom-file 'noerror)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; ELPA
-(when (require 'package nil :noerror)
-  (defun pre-install-packages (pkgs)
-    (let ((packages pkgs))
-      (mapc #'(lambda (pkg)
-                (unless (package-installed-p pkg)
-                  (package-install pkg)))
-            packages))))
-;;
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
-(package-initialize)
-(setq package-enable-at-startup nil)
-(when (not package-archive-contents)
-  (package-refresh-contents))
-;; default packages
-(pre-install-packages '(company company-quickhelp exec-path-from-shell
-                                fcitx flycheck
-                                flycheck-pos-tip helm helm-tramp
-                                nlinum-relative magit monokai-theme
-                                paredit powerline powerline-evil
-                                projectile rainbow-delimiters which-key
-                                yasnippet))
+;; Enable with t if you prefer
+(defconst *spell-check-support-enabled* nil)
+(defconst *is-mac-p* (eq system-type 'darwin))
+(defconst *is-windows-p* (eq system-type 'windows-nt))
+(defconst *is-linux-p* (eq system-type 'gnu/linux))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; My packages
-(add-to-list 'load-path (expand-file-name "els" user-emacs-directory))
+
+;;;; Temporarily reduce garbage collection during startup
+(defconst pufferfish/initial-gc-cons-threshold gc-cons-threshold
+  "Initial value of `gc-cons-threshold' at start-up time.")
+(setq gc-cons-threshold (* 128 1024 1024))
+(add-hook 'after-init-hook
+          (lambda () (setq gc-cons-threshold pufferfish/initial-gc-cons-threshold)))
+
+
+;;;; Bootstrap config
+(require 'init-utils)
+(require 'init-site-lisp)
+(require 'init-elpa)
+
+(require 'init-linum)
+(require 'init-evil)
+(require 'init-company)
+(require 'init-ivy)
+
+(require 'init-org)
+(require 'init-python)
+(require 'init-yaml)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; basic environment
-(when (eq system-type 'windows-nt)
+(when *is-windows-p*
   (setq tramp-default-method "plink")
   (setq default-directory "~/"))
 ;;
-(when (or (eq system-type 'darwin) (eq system-type 'gnu/linux))
-  (setq shell-file-name "/bin/zsh")
-  (exec-path-from-shell-initialize))
+(when (or *is-mac-p* *is-linux-p*)
+  (setq shell-file-name "/bin/zsh"))
+
+(when *is-mac-p*
+  (setq temporary-file-directory "/tmp/"))
+
+(global-set-key (kbd "C-c t l" ) 'toggle-truncate-lines)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; insert date and time
@@ -77,16 +86,11 @@
 ;;; color theme
 (load-theme 'monokai t)
 ;; (load-theme 'zenburn t)
+
 ;;; rainbow delimiters mode
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-;;; nlinum-relative
-(when (require 'nlinum-relative nil :noerror)
-  (nlinum-relative-setup-evil) ;; setup for evil
-  (add-hook 'prog-mode-hook 'nlinum-relative-mode)
-  (setq nlinum-relative-redisplay-delay 0) ;; delay
-  (setq nlinum-relative-current-symbol "") ;; or "" for display current line number
-  (setq nlinum-relative-offset 0)) ;; 1 if you want 0, 2, 3...
+
 ;;; highlight current line
 (global-hl-line-mode)
 ;;; config gui fonts
@@ -96,84 +100,15 @@
 (setq split-width-threshold nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; evil
-;;; install evil packages
-(pre-install-packages '(evil evil-leader evil-tabs))
-;;; evil configuration
-(when (require 'evil nil :no-error)
-  (require 'evil)
-  (evil-mode 1)
-  ;; (define-key evil-normal-state-map (kbd ",f") 'projectile-find-file)
-  ;; (define-key evil-normal-state-map (kbd ",,") 'evil-buffer)
-  (define-key evil-normal-state-map (kbd "q") nil)
-  ;;
-  (define-key evil-insert-state-map (kbd "C-e") nil)
-  (define-key evil-insert-state-map (kbd "C-d") nil)
-  (define-key evil-insert-state-map (kbd "C-k") nil)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  ;; (define-key evil-visual-state-map (kbd "C-c") 'evil-normal-state)
-  ;;
-  (define-key evil-motion-state-map (kbd "C-e") nil)
-  (define-key evil-visual-state-map (kbd "C-c") 'evil-exit-visual-state))
-;;; evil tab
-(when (require 'evil-tabs nil :no-error)
-  (global-evil-tabs-mode)
-  (evil-define-key 'normal evil-tabs-mode-map
-    "gnt" 'elscreen-create
-    "gkt" 'elscreen-kill))
-;;; evil leader
-(when (require 'evil-leader nil :no-error)
-  (setq evil-leader/in-all-states t)
-  (global-evil-leader-mode)
-  (evil-leader/set-key
-    ;; appearance
-    "mb" 'toggle-menu-bar-mode-from-frame
-    ;; helm
-    "x" 'helm-M-x
-    "f" 'helm-find-files
-    "b" 'helm-buffers-list
-    "k" 'kill-buffer
-    "y" 'helm-show-kill-ring
-    ;; projectile
-    "pf" 'project-find-file
-    ;; evil-tabs
-    "gt" 'elscreen-goto
-    ;; magit
-    "mgs" 'magit-status
-    "mgc" 'magit-commit
-    "mgt" 'magit-push
-    "mgl" 'magit-pull))
+;;;; which-key
+(when (require 'which-key nil :noerror)
+  (which-key-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; powerline
 (when (require 'powerline nil :noerror)
   (powerline-evil-vim-color-theme)
   (display-time-mode t))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; helm
-(require 'helm)
-(require 'helm-config)
-;;
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
-(global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-;; This is your old M-x.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-(helm-mode t)
-;; Add vim-like movement to Helm
-(define-key helm-map (kbd "C-j") 'helm-next-line)
-(define-key helm-map (kbd "C-k") 'helm-previous-line)
-(define-key helm-map (kbd "C-h") 'helm-next-source)
-(define-key helm-map (kbd "C-S-h") 'describe-key)
-(define-key helm-map (kbd "C-l") (kbd "RET"))
-(define-key helm-map [escape] 'helm-keyboard-quit)
-(dolist (keymap (list helm-find-files-map helm-read-file-map))
-  (define-key keymap (kbd "C-l") 'helm-execute-persistent-action)
-  (define-key keymap (kbd "C-h") 'helm-find-files-up-one-level)
-  (define-key keymap (kbd "C-S-h") 'describe-key))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; paredit
@@ -240,27 +175,6 @@
   (with-eval-after-load 'flycheck (flycheck-pos-tip-mode)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; org mode
-;; key binding
-(define-key global-map "\C-cc" 'org-capture)
-;; capture templates
-(setq org-capture-templates
-     '(("j" "Journal" entry (file+datetree "~/Dropbox/ORG/journal.org")
-        "* %?\n Entered On: %U\n %i\n %a")))
-;;; org sync with google calendar
-(pre-install-packages '(calfw org-gcal))
-;;
-(when (and (require 'calfw nil :noerror)
-           (require 'calfw-org nil :noerror)
-           (require 'org-gcal nil :noerror))
-  (setq org-gcal-client-id "something.apps.googleusercontent.com"
-        org-gcal-client-secret "something"
-        org-gcal-file-alist '(("lizhikun@growing.io" .  "~/Dropbox/ORG/gcal.org"))))
-;; org export setting
-(setq org-export-backends '(ascii html icalendar latex md freemind))
-(setq org-export-html-coding-system 'utf-8)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; iimage mode
 (when (require 'iimage nil :noerror)
   (autoload 'iimage-mode "iimage" "Support Inline image minor mode." t)
@@ -276,7 +190,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Rust IDE
 ;;; install rust ide packages
-(pre-install-packages '(cargo company-racer flycheck-rust racer rust-mode))
 ;;; config rust ide
 ;; rust-mode
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
@@ -299,9 +212,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Clojure IDE
-;;; install clojure ide packages
-(pre-install-packages '(cider clojure-cheatsheet clojure-mode flycheck-clojure))
-;;; config clojure ide
 ;; eldoc setting for clojure
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
@@ -314,41 +224,3 @@
 ;; flycheck setting for clojure
 (add-hook 'clojure-mode-hook #'flycheck-mode)
 (eval-after-load 'flycheck '(flycheck-clojure-setup))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; python IDE
-;;; install python ide packages
-(pre-install-packages '(anaconda-mode company-anaconda elpy py-yapf))
-;; python indent
-(add-hook 'python-mode-hook
-	      (lambda ()
-		    (setq-default indent-tabs-mode t)
-		    (setq-default tab-width 4)
-		    (setq-default py-indent-tabs-mode t)
-			(setq-default python-indent-offset 4)
-	        (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
-;;; config python ide
-(elpy-enable)
-(setq python-shell-completion-native-enable nil)
-;; pyvenv
-(when (require 'pyvenv nil :noerror)
-  (cond
-   ;; python virtualenv workon directory
-   ((or (eq system-type 'gnu/linux)
-        (eq system-type 'darwin))
-    (setenv "WORKON_HOME" (expand-file-name "opt/python-venv"
-                                            (getenv "HOME"))))
-   ((eq system-type 'windwos-nt)
-    (setenv "WORKON_HOME" "D:/opt/Python/venv"))))
-;; anaconda
-(add-hook 'python-mode-hook #'anaconda-mode)
-(add-hook 'python-mode-hook #'anaconda-eldoc-mode)
-;; flycheck
-(add-hook 'python-mode-hook #'flycheck-mode)
-;; py-autopep8
-(add-hook 'python-mode-hook 'py-yapf-enable-on-save)
-;; company
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-anaconda))
-;; python inferior completion
-(add-hook 'inferior-python-mode-hook #'company-mode)
